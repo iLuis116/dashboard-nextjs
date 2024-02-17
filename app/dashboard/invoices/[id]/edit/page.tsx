7612e9f630ae2e121c328e3b2b6c8c809249d94e
iLuis116 <84213931+iLuis116@users.noreply.github.com>
2024-02-17 01:20:32 -0600

    Pruebas 2

--- app/dashboard/invoices/[id]/edit/page.tsx
+++ app/dashboard/invoices/[id]/edit/page.tsx
@@ -7,7 +7,6 @@ import { Metadata } from 'next';
 export const metadata: Metadata = {
   title: 'Edit',
 };
-
 export default async function Page({ params }: { params: { id: string } }) {
     const id = params.id;
     const [invoice, customers] = await Promise.all([
